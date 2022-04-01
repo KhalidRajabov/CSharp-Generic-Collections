92c7976ea720fd93029733ce9259a7650c36a057
dbBurzum <khalidsr@code.edu.az>
2022-04-01 16:49:21 +0400

    Add project files.

---
+++ CSharp-Generic-Collections/Models/Group.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserAccount.Models
+{
+    internal class Group
+    {
+        private string _groupno;
+        private int _stdlimit;
+        private Student[] students;
+        public int StdLimit
+        {
+            get
+            {
+                return _stdlimit;
+            }
+            set
+            {
+                if (value > 1 && value < 18) // minimum valueni test meqsedli 1 yazmisham
+                {
+                    _stdlimit = value;
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Min student limit 5, max student limit 18");
+                }
+            }
+        }
+        public string GroupNo
+        {
+            get
+            {
+                return _groupno;
+            }
+            set
+            {
+                if (CheckGroupNo(value) == true)
+                {
+                    _groupno = value;
+                    Console.WriteLine($"Your GroupNo: {GroupNo}");
+                    return;
+                }
+
+            }
+        }
+        public Group(string groupno, int stdlimit)
+        {
+            students = new Student[0];
+            GroupNo = groupno;
+            StdLimit = stdlimit;
+        }
+        public void AddStudent(Student std)
+        {
+
+            if (students.Length < StdLimit)
+            {
+                Array.Resize(ref students, students.Length + 1);
+                students[students.Length - 1] = std;
+                Console.WriteLine($"{std.WholeName} added to group: {GroupNo}.");
+                return;
+            }
+        }
+        public bool CheckGroupNo(string groupNo)
+        {
+            bool final = false;
+            bool let = false;
+            bool num = false;
+            if (groupNo.Length == 5)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    let = false;
+                    if (char.IsUpper(groupNo[i]) == true)
+                    {
+                        let = true;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("First 2 letters of GroupNo must be in uppercase.");
+                        break;
+                    }
+                }
+                for (int i = 2; i < groupNo.Length; i++)
+                {
+                    num = false;
+                    if (char.IsNumber(groupNo[i]) == true)
+                    {
+                        num = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("GroupNo must have numbers in it.");
+                        break;
+                    }
+                }
+                if (let == true && num == true)
+                {
+                    final = true;
+                }
+                else if (num == false || let == false)
+                {
+                    final = false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("GroupNo must have 5 characters: 2 uppercase and 3 numbers");
+            }
+            return final;
+
+        }
+
+        public void GetStudent(int? id)
+        {
+            bool exist = false;
+            foreach (Student std in students)
+            {
+                if (id == std.Ident)
+                {
+                    Console.WriteLine(std);
+                    exist = true;
+                    return;
+                }
+            }
+            if (exist == false)
+            {
+                Console.WriteLine($"{id} could not be found in {GroupNo}");
+            }
+        }
+        public void GetStudents()
+        {
+            foreach (Student std in students)
+            {
+                Console.WriteLine(std);
+            }
+        }
+    }
+
+
+}
---
+++ CSharp-Generic-Collections/Models/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserAccount.Models
+{
+    internal class Student
+    {
+        private static int _ident = 0;
+        public int Ident { get; }
+        public string WholeName { get; set; }
+        private int _point;
+        public int Point
+        {
+            get
+            {
+                return _point;
+            }
+            set
+            {
+                if (value > 100 || value < 1)
+                {
+                    Console.WriteLine("Points can not be 0 or more than 100 or less than 0");
+                    return;
+                }
+                else
+                {
+
+                    _point = value;
+                    _ident++;
+                    StudentInfo();
+                    return;
+                }
+
+            }
+        }
+        public Student()
+        {
+            Ident = _ident;
+        }
+        public Student(string name, int point)
+        {
+            WholeName = name;
+            Point = point;
+        }
+        public void StudentInfo()
+        {
+            Console.WriteLine($"\n" +
+                $"Student Id number: {_ident} \n" +
+                $"Student name: {WholeName}\n" +
+                $"Studnet point: {Point} \n" +
+                $"\n" +
+                $"");
+        }
+
+    }
+}
